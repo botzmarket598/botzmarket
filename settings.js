49b2fb2036fc43185949ecabaaa9958331060687
🦠𝑩𝑶𝑻𝒁𝑴𝑨𝑹𝑲𝑬𝑻🦠 <rizkygpt92@gmail.com>
2025-11-15 23:07:17 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,18 @@
+//========= botzmarket =========//
+const settings = {
+  token: 'Ganti dengan token punya kamu',
+  adminId: '8305321213',
+  
+
+//========= botzmarket =========//
+  pp: 'https://files.catbox.moe/eu0sp3.jpg',
+  urladmin: 'https://t.me/botzmarket95',
+  domain: 'ganti dengan domain panel kamu',
+  plta: 'ptla',
+  pltc: 'ptlc',
+  loc: '1',
+  eggs: '15'
+};
+//========= botzmarket =========//
+
+module.exports = settings; 
